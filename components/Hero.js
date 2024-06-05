3043aa8697916bdcfd9367d8cbf7782b1858592f
Dave <davkena@gmail.com>
2024-06-05 17:24:02 -0600

    login page

--- components/Hero.js
+++ components/Hero.js
@@ -1,11 +1,18 @@
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 
 const Hero = () => {
+    const router = useRouter();
+    const handleLoginClick = () => {
+        router.push('/login');
+    };
     return (
-        <div className="bg-blue-500 text-white flex flex-col items-center justify-center h-screen relative overflow-hidden bg-cover bg-center animate-slideUp" style={{ backgroundImage: "url('/background.png')" }}>
+        <div className="text-white flex flex-col items-center justify-center h-screen relative overflow-hidden bg-cover bg-center animate-slideUp" style={{ backgroundImage: "url('/background.png')" }}>
             <h1 className="text-5xl font-bold mb-4 text-shadow">Your Assessment Hub</h1>
             <p className="text-xl mb-8 text-shadow">Please login to continue</p>
-            <button className="bg-white text-blue-500 px-4 py-2 rounded-full hover:bg-gray-200 transition">
+            <button
+                onClick={handleLoginClick}
+                className="bg-white text-black px-4 py-2 rounded-full hover:bg-gray-200 transition">
                 LOGIN
             </button>
         </div>
--- pages/login.js
+++ pages/login.js
@@ -1,44 +1,40 @@
-// pages/login.js
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { login } from '../utils/auth';
+import Head from 'next/head';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await login(username, password);
-      router.push('/');
-    } catch (error) {
-      console.error('Failed to login:', error);
-    }
-  };
-
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+    <>
+      <Head>
+        <title>Login Page</title>
+      </Head>
+      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="login-container bg-blue-200 bg-opacity-90 p-8 rounded-lg shadow-md text-center max-w-md w-full">
+        <div className="flex flex-col items-center mb-5 ">
+          <FontAwesomeIcon icon={faBookOpen} size="4x" className="text-slate-800 mb-3" style={{ color: '#1E3A8A' }} />
+          <h1 className="text-2xl font-bold text-slate-800">Your Assessment Hub</h1>
+        </div>
         <input
           type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          required
+          placeholder="Username/Email"
+          className="w-full p-3 mb-4 border font-bold text-slate-800 border-gray-300 rounded"
         />
         <input
           type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
           placeholder="Password"
-          required
+          className="w-full p-3 mb-4 border font-bold text-slate-800 border-gray-300 rounded"
         />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+        <label className="block mb-4 text-gray-700">
+          <input type="checkbox" className="mr-2" /> Remember Me
+        </label>
+        <button className="w-full p-3 bg-blue-900 font-bold text-white rounded hover:bg-blue-700">
+          Login
+        </button>
+        <Link href="#" className="block mt-3 font-bold text-slate-800 hover:underline">Forgot Password?</Link>
+        </div>
+      </div>
+    </>
   );
 };
 
